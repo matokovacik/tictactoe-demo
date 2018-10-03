0786afb3f16a718baa89da628dc6f7c31c9a1088
Martin Kovacik <mato.kovacik@gmail.com>
2018-10-03 21:18:29 +0200

    Added test for invalid moves

--- game/src/test/java/tictactoe/TicTacToeTest.java
+++ game/src/test/java/tictactoe/TicTacToeTest.java
@@ -19,8 +19,14 @@ public class TicTacToeTest {
     }
     
     @Test(expected = InvalidMoveException.class) 
-    public void validateConsecutiveStep() {
+    public void validateConsecutiveStep1() {
         game.playX(1);
         game.playX(2);
+    }
+
+    @Test(expected = InvalidMoveException.class)
+    public void validateConsecutiveStep2() {
+        game.playO(1);
+        game.playO(2);
     }
 }
